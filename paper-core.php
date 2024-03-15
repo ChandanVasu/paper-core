f19a69a353d5736c949e0bcb0d961b60e21a83a9
Chandan Vasu <chandanyadavparas@gmail.com>
2024-03-15 17:31:00 +0530

    first commit

---
+++ paper-core.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Paper Core
+Description: Paper Core Plugin for the theme.
+Version: 1.0
+Author: Vasu Theme
+Author URI: http://vasutheme.mjnnu.com/
+*/
+
+
+// Include custom post type class
+require_once plugin_dir_path(__FILE__) . 'Inc/paper-template.php';
+// Include meta boxes class
+
+
